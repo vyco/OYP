ee15d6451afa7fdc2224447d16d9f22130b16d49
AishaLawal <noBeast21@mail.com>
2017-08-01 15:12:41 +0100

    fadeIns

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
 $(document).ready(function(){
   $('.main-body').css('display','none');
   $('.enter').click(function(){
-  $("#enterSite").css('opacity','0');
+
   $('#enterSite').css('display','none');
-  $('.main-body').css('opacity','1');
   $('.main-body').css('display','block');
   });
   selector();
   nav();
+  fader();
 });
 function selector(){
   $('.editorials').css('display','none');
@@ -19,8 +19,29 @@ $('.flex-unit[data-id=1]').click(function(){
 }
 
 function nav(){
+
+  $('.back').click(function(event){
+    event.preventDefault();
+    $('#enterSite').css('display','block');
+    $('.main-body').css('display','none');
+  });
   $('.fa-arrow-left').click(function(){
     $('.editorials').css('display','none');
     $('.main-body').css('display','block');
   });
+}
+
+function fader(){
+  $('.item').each(function(i){
+    setTimeout(function(){
+        $('.item').eq(i).addClass("animated");
+        $('.item').eq(i).addClass("fadeIn");
+    },i*400);
+});
+$('.pic-small').each(function(i){
+  setTimeout(function(){
+      $('.pic').eq(i).addClass("animated");
+      $('.pic').eq(i).addClass("fadeIn");
+  },i*2000);
+});
 }
